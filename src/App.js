ce6ff45dc6c3ce9771389787d231515e9e366b74
Savan-Solanki <savanj333@gmail.com>
2025-02-13 10:14:20 +0530

    Added googleAuth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import Realtime from './Realtime.js'
-import Store from './Store.js'
+// import Realtime from './Realtime.js'
+// import Store from './Store.js'
+import GoogleAuth from './GoogleAuth'
 
 export default function App() {
   return (
     <div>
-      <Realtime/>
-      <Store/>
+      {/* <Realtime/>
+      <Store/> */}
+      <GoogleAuth/>
     </div>
   )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,29 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBSW8-sRqh9_nMtaW82J7snHjiqpUUXXZk",
+//   authDomain: "realtimefirebase-16445.firebaseapp.com",
+//   projectId: "realtimefirebase-16445",
+//   storageBucket: "realtimefirebase-16445.firebasestorage.app",
+//   messagingSenderId: "758718392725",
+//   appId: "1:758718392725:web:73cd4e1f65fd10c1592e9c",
+//   measurementId: "G-1F2VF2MF39"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+
+// For Google Auth
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -7,15 +33,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBSW8-sRqh9_nMtaW82J7snHjiqpUUXXZk",
-  authDomain: "realtimefirebase-16445.firebaseapp.com",
-  projectId: "realtimefirebase-16445",
-  storageBucket: "realtimefirebase-16445.firebasestorage.app",
-  messagingSenderId: "758718392725",
-  appId: "1:758718392725:web:73cd4e1f65fd10c1592e9c",
-  measurementId: "G-1F2VF2MF39"
+  apiKey: "AIzaSyA0hlXVMqhzKWQdXCS2-r1oI4vzAIXjv_A",
+  authDomain: "auth-3cb47.firebaseapp.com",
+  projectId: "auth-3cb47",
+  storageBucket: "auth-3cb47.firebasestorage.app",
+  messagingSenderId: "1057456168860",
+  appId: "1:1057456168860:web:0f0e8062dbd729e2e6f259",
+  measurementId: "G-DN9E694ZBH"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
